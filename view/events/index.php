f69e7443f9345c3af273d8049ab7c50c4385349d
ImLuze <luzedz@gmail.com>
2018-08-20 20:03:41 +0200

    Added basic front-end news section to homepage

--- view/events/index.php
+++ view/events/index.php
@@ -16,4 +16,44 @@
       <a class="cta-primary home-tripel-cta" href="#">Bestel uw Tripel</a>
     </div>
   </section>
+
+  <section class="news">
+    <h2>Meest recente nieuws</h2>
+    <div class="news-cards">
+
+      <article class="news-card">
+        <div class="gradient news-gradient-1">
+        </div>
+        <div>
+          <h3>15 Architecturale toppers</h3>
+          <p class="news-date">16.08.18</p>
+          <p class="home-intro news-intro">Na een geslaagde samenwerking met het VAi, het Vlaams Architectuurinstituut, in 2017...</p>
+          <a class="cta-secondary news-cta" href="#">LEES MEER</a>
+        </div>
+      </article>
+
+      <article class="news-card">
+        <div class="gradient news-gradient-2">
+        </div>
+        <div>
+          <h3>100 jaar oorlog op 8 plekken</h3>
+          <p class="news-date">12.08.18</p>
+          <p class="home-intro news-intro">Op deze 8 plaatsen in Vlaanderen word je op zondag 9 september even stil...</p>
+          <a class="cta-secondary news-cta" href="#">LEES MEER</a>
+        </div>
+      </article>
+
+      <article class="news-card">
+        <div class="gradient news-gradient-3">
+        </div>
+        <div>
+          <h3>11 kastelen om in weg te dromen</h3>
+          <p class="news-date">09.08.18</p>
+          <p class="home-intro news-intro">Bezoek één van deze 11 indrukwekkende kastelen en droom weg...</p>
+          <a class="cta-secondary news-cta" href="#">LEES MEER</a>
+        </div>
+      </article>
+
+    </div>
+  </section>
 </main>
