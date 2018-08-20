0aa22184166732560326862628f043af6a7809d9
ImLuze <luzedz@gmail.com>
2018-08-20 17:27:54 +0200

    Added tripel section

--- view/events/index.php
+++ view/events/index.php
@@ -2,7 +2,18 @@
     <div class="home-header-container">
         <h1 class="home-header-title">Open</br>Monumenten</br>dag</h1>
         <p class="home-header-date">09.09.18</p>
-        <p class="home-header-intro">Open Monumentendag, dat is het grootste culturele eendagsevenement van Vlaanderen. Op de 2de zondag van september stellen honderden monumenten in heel Vlaanderen gratis hun deuren open.</p>
-        <a class="cta-primary" href="#">Alle monumenten</a>
+        <p class="home-intro home-header-intro">Open Monumentendag, dat is het grootste culturele eendagsevenement van Vlaanderen. Op de 2de zondag van september stellen honderden monumenten in heel Vlaanderen gratis hun deuren open.</p>
+        <a class="cta-primary home-header-cta" href="#">Alle monumenten</a>
     </div>
 </header>
+
+<main>
+  <section class="tripel">
+    <img class="tripel-img" src="../assets/svg/beer.svg" alt="Tripel bier" width="271" height="436">
+    <div class="tripel-copy">
+      <h2 class="tripel-title">Feest mee met een lekkere Tripel</h2>
+      <p class="home-intro home-tripel-intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <a class="cta-primary home-tripel-cta" href="#">Bestel uw Tripel</a>
+    </div>
+  </section>
+</main>
