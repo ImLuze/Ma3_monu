e2c5281198007365e722eef1390acbaa95b6881f
ImLuze <luzedz@gmail.com>
2018-08-21 15:23:45 +0200

    Added entire events page

--- view/events/events.php
+++ view/events/events.php
@@ -19,3 +19,66 @@
     </article>
   <? endforeach;?>
 </section> -->
+
+<header class="filter-container">
+  <form class="filter" action="index.php?page=events" method="post">
+    <input class="input" type="text" name="name" value="" placeholder="Naam monument">
+    <input class="input" type="text" name="place" value="" placeholder="Gemeente">
+    <select class="input" name="tag">
+      <option value="no tag">Activiteit of type</option>
+      <option value="tag">Tag</option>
+      <option value="other tag">Other Tag</option>
+    </select>
+  </form>
+</header>
+
+<main>
+  <section class="event-cards">
+
+    <article class="event-card">
+      <div class="event-card-img" style="background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(237, 167, 198, 1) 100%), url(../assets/img/events/CAK/c72146d8-005f-11e8-8ba7-02b7b76bf47f_0.jpg); background-blend-mode: color-burn; background-size: cover;"></div>
+      <p class="event-card-title">Het kasteel blauwhuis</p>
+    </article>
+
+    <article class="event-card">
+      <div class="event-card-img" style="background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(237, 167, 198, 1) 100%), url(../assets/img/events/CAK/c72146d8-005f-11e8-8ba7-02b7b76bf47f_0.jpg); background-blend-mode: color-burn; background-size: cover;"></div>
+      <p class="event-card-title">Het kasteel blauwhuis</p>
+    </article>
+
+    <article class="event-card">
+      <div class="event-card-img" style="background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(237, 167, 198, 1) 100%), url(../assets/img/events/CAK/c72146d8-005f-11e8-8ba7-02b7b76bf47f_0.jpg); background-blend-mode: color-burn; background-size: cover;"></div>
+      <p class="event-card-title">Het kasteel blauwhuis</p>
+    </article>
+
+    <article class="event-card">
+      <div class="event-card-img" style="background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(237, 167, 198, 1) 100%), url(../assets/img/events/CAK/c72146d8-005f-11e8-8ba7-02b7b76bf47f_0.jpg); background-blend-mode: color-burn; background-size: cover;"></div>
+      <p class="event-card-title">Het kasteel blauwhuis</p>
+    </article>
+
+    <article class="event-card">
+      <div class="event-card-img" style="background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(237, 167, 198, 1) 100%), url(../assets/img/events/CAK/c72146d8-005f-11e8-8ba7-02b7b76bf47f_0.jpg); background-blend-mode: color-burn; background-size: cover;"></div>
+      <p class="event-card-title">Het kasteel blauwhuis</p>
+    </article>
+
+    <article class="event-card">
+      <div class="event-card-img" style="background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(237, 167, 198, 1) 100%), url(../assets/img/events/CAK/c72146d8-005f-11e8-8ba7-02b7b76bf47f_0.jpg); background-blend-mode: color-burn; background-size: cover;"></div>
+      <p class="event-card-title">Het kasteel blauwhuis</p>
+    </article>
+
+    <article class="event-card">
+      <div class="event-card-img" style="background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(237, 167, 198, 1) 100%), url(../assets/img/events/CAK/c72146d8-005f-11e8-8ba7-02b7b76bf47f_0.jpg); background-blend-mode: color-burn; background-size: cover;"></div>
+      <p class="event-card-title">Het kasteel blauwhuis</p>
+    </article>
+
+    <article class="event-card">
+      <div class="event-card-img" style="background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(237, 167, 198, 1) 100%), url(../assets/img/events/CAK/c72146d8-005f-11e8-8ba7-02b7b76bf47f_0.jpg); background-blend-mode: color-burn; background-size: cover;"></div>
+      <p class="event-card-title">Het kasteel blauwhuis</p>
+    </article>
+
+    <article class="event-card">
+      <div class="event-card-img" style="background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(237, 167, 198, 1) 100%), url(../assets/img/events/CAK/c72146d8-005f-11e8-8ba7-02b7b76bf47f_0.jpg); background-blend-mode: color-burn; background-size: cover;"></div>
+      <p class="event-card-title">Het kasteel blauwhuis</p>
+    </article>
+
+  </section>
+</main>
