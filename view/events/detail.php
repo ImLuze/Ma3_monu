57963c7376d777df9eea3056f31c0a61b558607b
ImLuze <luzedz@gmail.com>
2018-08-22 11:59:57 +0200

    Made detailpage responsive

--- view/events/detail.php
+++ view/events/detail.php
@@ -1,19 +1,21 @@
 <header class="detail-header">
   <section class="slider">
     <div class="slider-container">
-      <div class="slider-img gradient slider-gradient" style="background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(237, 167, 198, 1) 100%), url(../assets/img/events/CAK/c72146d8-005f-11e8-8ba7-02b7b76bf47f_0.jpg); background-blend-mode: color-burn; background-size: cover;"></div>
+      <div class="slider-img slider-gradient" style="background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(237, 167, 198, 1) 100%), url(../assets/img/events/CAK/c72146d8-005f-11e8-8ba7-02b7b76bf47f_0.jpg); background-blend-mode: color-burn; background-size: cover;"></div>
     </div>
     <div class="slider-controls">
       <button class="slider-controls-button slider-controls-button-active"></button>
     </div>
   </section>
   <section class="detail-header-intro">
-    <h1><?php echo($event['title'])?></h1>
-    <div class="detail-header-sub">
-      <p class="home-header-date"><?php echo($event['city'])?></p>
-      <a class="cta-primary" href=<?php echo($event['link'])?>>Website</a>
+    <div class="detail-header-intro-top">
+      <h1 class="detail-header-title"><?php echo($event['title'])?></h1>
+      <div class="detail-header-sub">
+        <p class="home-header-date"><?php echo($event['city'])?></p>
+        <a class="cta-primary" href=<?php echo($event['link'])?>>Website</a>
+      </div>
     </div>
-    <p class="home-intro"><?php echo($event['content'])?></p>
+    <p class="home-intro half-intro"><?php echo($event['content'])?></p>
   </section>
 </header>
 
@@ -26,17 +28,17 @@
     </div>
     <div class="detail-walloftext">
       <h2>Geschiedenis</h2>
-      <p class="home-intro detail-intro"><?php echo($event['geschiedenis'])?></p>
+      <p class="home-intro detail-intro half-intro"><?php echo($event['geschiedenis'])?></p>
     </div>
   </section>
   <section class="detail-right">
     <div class="detail-praktisch">
       <h2>Praktisch</h2>
-      <p class="home-intro detail-intro"><?php echo($event['praktisch']) ?></p>
+      <p class="home-intro detail-intro half-intro"><?php echo($event['praktisch']) ?></p>
     </div>
     <div class="detail-walloftext">
       <h2>Activiteiten</h2>
-      <p class="home-intro detail-intro"><?php echo($event['activiteiten']) ?></p>
+      <p class="home-intro detail-intro half-intro"><?php echo($event['activiteiten']) ?></p>
     </div>
   </section>
 </main>
--- view/events/index.php
+++ view/events/index.php
@@ -3,7 +3,7 @@
         <h1 class="home-header-title">Open</br>Monumenten</br>dag</h1>
         <p class="home-header-date">09.09.18</p>
         <p class="home-intro home-header-intro">Open Monumentendag, dat is het grootste culturele eendagsevenement van Vlaanderen. Op de 2de zondag van september stellen honderden monumenten in heel Vlaanderen gratis hun deuren open.</p>
-        <a class="cta-primary home-header-cta" href="#">Alle monumenten</a>
+        <a class="cta-primary home-header-cta" href="/index.php?page=events">Alle monumenten</a>
     </div>
 </header>
 
